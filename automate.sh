f58a85ba6f44b627fc64a12e7de49f1c7332b40c
vinaykumar721 <gamanab163@msitprogram.net>
2020-10-01 07:43:45 +0530

    first commit

---
+++ automate.sh
@@ -0,0 +1,19 @@
+function automate(){ 
+pwd
+mkdir MSIT
+cd MSIT
+pwd
+cd -
+mkdir IIIT-H
+cd IIIT-H
+touch telugu.txt
+touch hindi.txt
+touch english.txt
+touch maths.txt
+touch science.txt
+ls
+pwd
+cd -
+ls -l
+}
+automate
